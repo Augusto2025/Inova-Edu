01430dd0b3600fcb12931beb116c78ef9ff4b799
augusto <augustogabrielmsousa@gmail.com>
2026-02-25 14:31:25 -0300

    "Tirando os nomes para o .env"

--- Desktop/config/banco.py
+++ Desktop/config/banco.py
@@ -15,11 +15,11 @@ socket.setdefaulttimeout(3)
 
 def conectar():
     """Conecta ao banco com timeout real de 3 segundos"""
-    host = os.getenv("HOST", "localhost")
-    user = os.getenv("USER", "root")
-    password = os.getenv("PASSWORD", "182105")
-    database = os.getenv("NAME", "db_repositorio")
-    port = int(os.getenv("PORT", "3306"))
+    host = os.getenv("HOST")
+    user = os.getenv("USER")
+    password = os.getenv("PASSWORD")
+    database = os.getenv("NAME")
+    port = int(os.getenv("PORT"))
 
     # Teste rápido de socket TCP para falha imediata
     try:
