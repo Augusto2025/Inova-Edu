55a5a92a8f4f548dcd5ee370037453457b7ad9f3
augusto <augustogabrielmsousa@gmail.com>
2026-04-01 15:34:32 -0300

    "Criando a tela de login"

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// importe de navegação
+// instalação do react navigation: npm install @react-navigation/native
+// instalação do stack navigator: npm install @react-navigation/native-stack
+// instalação de dependências: npm install react-native-screens react-native-safe-area-context
+// instalação via Expo: npx expo install react-native-screens react-native-safe-area-context
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// importando telas
+import LoginScreen from './src/screens/Login';
+
+// criando o stack de navegação (pilha de telas)
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      {/* stack navigator, usando screenOptions para ocultar o cabeçalho */}
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+
+      </Stack.Navigator>
+    
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
