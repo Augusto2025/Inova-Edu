3c5b2df37ab6cc4faf5118f08682d73247a1eb10
Clariane Torres <Claryazevedo18@gmail.com>
2026-04-07 20:48:51 -0300

    "..................."

--- Mobile/App.js
+++ Mobile/App.js
@@ -24,13 +24,13 @@ export default function App() {
 
       {/* stack navigator, usando screenOptions para ocultar o cabeçalho */}
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Forum" component={forumScreen}/>
         
+        <Stack.Screen name="Calendar" component={CalendarScreen}/>
         <Stack.Screen name="footerforum" component={FooterForum}/>  
         <Stack.Screen name="headerforum" component={HeaderForum}/>
-        <Stack.Screen name="Calendar" component={CalendarScreen}/>
         <Stack.Screen name="Splash" component={SplashScreen}/>
         <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Forum" component={forumScreen}/>
         {/* <Stack.Screen name="Home" component={HomeScreen}/> */}
 
       </Stack.Navigator>
