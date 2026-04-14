f58b4d7070c4cb16a013df320d9d8728eeeb1ac1
augusto <augustogabrielmsousa@gmail.com>
2026-04-14 14:55:34 -0300

    "Ajuste de conectividade entre telas"

--- Mobile/App.js
+++ Mobile/App.js
@@ -14,6 +14,7 @@ import CursosScreen from './src/screens/Cursos';
 import TurmasScreen from './src/screens/Turmas';
 import ProjetosScreen from './src/screens/Projetos';
 import RepositorioScreen from './src/screens/Repositorio';
+import CalendarScreen from './src/screens/Eventos';
 
 // STACK
 const Stack = createStackNavigator();
@@ -22,23 +23,27 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Cursos" component={CursosScreen}/>
+        <Stack.Screen name="Turmas" component={TurmasScreen}/>
+        <Stack.Screen name="Projetos" component={ProjetosScreen}/>
+        <Stack.Screen name="Repositorio" component={RepositorioScreen}/>
+
         <Stack.Screen name="Splash" component={SplashScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
         
         <Stack.Screen name="Titulo" component={TituloScreen} />
         <Stack.Screen name="Forum" component={ForumScreen}/>
         <Stack.Screen name="Conversa" component={ConversaScreen} />
 
-        <Stack.Screen name="Repositorio" component={RepositorioScreen}/>
-        <Stack.Screen name="Projetos" component={ProjetosScreen}/>
-        <Stack.Screen name="Turmas" component={TurmasScreen}/>
-        <Stack.Screen name="Cursos" component={CursosScreen}/>
-        <Stack.Screen name="Profile" component={ProfilePage}/>
-        <Stack.Screen name="Calendar" component={CalendarScreen}/>
-        <Stack.Screen name="footerforum" component={FooterForum}/>  
-        <Stack.Screen name="headerforum" component={HeaderForum}/>
 
-        <Stack.Screen name="Splash" component={SplashScreen}/>
-        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Eventos" component={CalendarScreen}/>
+
+        <Stack.Screen name="Profile" component={ProfilePage}/>
+
+        {/* <Stack.Screen name="footerforum" component={FooterForum}/>  
+        <Stack.Screen name="headerforum" component={HeaderForum}/> */}
+
+        {/* <Stack.Screen name="Splash" component={SplashScreen}/> */}
 
       </Stack.Navigator>
     </NavigationContainer>
