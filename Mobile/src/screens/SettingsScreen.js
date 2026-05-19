77534fa2c349743f2117c6f2ff81c54b1746cb1f
Clariane <claryazevedo18@gmail.com>
2026-05-19 16:49:50 -0300

    "versão"

--- Mobile/src/screens/SettingsScreen.js
+++ Mobile/src/screens/SettingsScreen.js
@@ -260,6 +260,23 @@ export default function ConfiguracoesScreen({ navigation }) {
 
         </View>
 
+        {/* Versão */}
+
+        <Text style={styles.titulo}>
+          Versão
+        </Text>
+
+        <View style={styles.card}>
+          <Text style={{ color: "#777" }}>
+            Versão Brasileira: Herbert Richers 1.0.0
+          </Text>
+
+        </View>
+
+
+
+
+
         <TouchableOpacity
           style={styles.logout}
         >
