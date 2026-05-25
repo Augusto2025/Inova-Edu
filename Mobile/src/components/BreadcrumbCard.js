41e91cd540a4bcf9c1e9018e869b4fee17ef5a1d
augusto <augustogabrielmsousa@gmail.com>
2026-05-25 17:19:02 -0300

    "Adicionando o style ao botão de download"

--- Mobile/src/components/BreadcrumbCard.js
+++ Mobile/src/components/BreadcrumbCard.js
@@ -11,16 +11,16 @@ export default function BreadcrumbCard({ titulo, itemSub, botaoAcao, aoPressiona
       <View style={styles.breadcrumbInfo}>
         <Text style={styles.itemSub}>{itemSub}</Text>
         <Text style={styles.textPrimary}>{titulo}</Text>
-        {botaoAcao && (
-          <TouchableOpacity 
-            style={styles.btnActionMain} 
-            onPress={aoPressionar}
-            activeOpacity={0.7}
-          >
-            <Feather name={iconeBotao} size={20} color="white"/>
-          </TouchableOpacity>
-        )}
       </View>
+      {botaoAcao && (
+        <TouchableOpacity 
+          style={styles.btnActionMain} 
+          onPress={aoPressionar}
+          activeOpacity={0.7}
+        >
+          <Feather name={iconeBotao} size={20} color="white"/>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -40,6 +40,7 @@ const styles = StyleSheet.create({
   breadcrumbInfo: { // Adicionado para evitar quebra caso use propriedades dele
     flexDirection: 'column',
   },
+  btnActionMain: { marginLeft: '30%', backgroundColor: COLORS.accent, width: 45, height: 45, borderRadius: 12, justifyContent: 'center', alignItems: 'center' },
   textPrimary: { 
     fontSize: 14, 
     fontWeight: 'bold', 
