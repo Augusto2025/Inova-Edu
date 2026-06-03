8c3ecee49b12859c4d6dfc2704c6ede621767f86
Clariane <claryazevedo18@gmail.com>
2026-06-03 14:45:25 -0300

    "conflito do header"

--- Mobile/src/components/Header.js
+++ Mobile/src/components/Header.js
@@ -100,7 +100,6 @@ export default function Header({
             <View style={styles.badge}>
               <Text style={styles.badgeText}>{quantidadeNotificacoes}</Text>
             </View>
-<<<<<<< HEAD
           )}
         </AnimatedTouchableOpacity>
       </View>
@@ -111,9 +110,6 @@ export default function Header({
         </View>
       )}
     </View>
-=======
-          </View>
->>>>>>> fe8ed333d8f939bf6a5d687478470cb14e95227e
   );
 }
 
