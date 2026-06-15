8a77fdd377ab0367caf18848603fbd5bb9de6036
augusto <augustogabrielmsousa@gmail.com>
2026-06-15 17:01:24 -0300

    "Correção do caminho da rota no server"

--- Mobile/backend/server.js
+++ Mobile/backend/server.js
@@ -9,15 +9,17 @@ const authRoutes = require('./routes/login');
 const cursosRoutes = require('./routes/cursos');
 const turmasRoutes = require('./routes/turmas');
 const projetosRoutes = require('./routes/projetos');
+const repositorioRoutes = require('./routes/repositorio');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // 2. Encaminha as chamadas para os arquivos certos
-app.use('/login', authRoutes);   // Tudo sobre login
-app.use('/cursos', cursosRoutes); // LINHA NOVA: Tudo sobre cursos
-app.use('/turmas', turmasRoutes); // LINHA NOVA: Tudo sobre turmas
-app.use('/projetos', projetosRoutes); // LINHA NOVA: Tudo sobre projetos
+app.use('/login', authRoutes);
+app.use('/cursos', cursosRoutes); 
+app.use('/turmas', turmasRoutes);
+app.use('/projetos', projetosRoutes); 
+app.use('/repositorio', repositorioRoutes); 
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
