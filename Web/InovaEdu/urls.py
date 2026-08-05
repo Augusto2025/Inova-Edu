4b9faabd6b24bbe0fb31dbc67b8306bd3008fe8f
Clariane <claryazevedo18@gmail.com>
2026-08-05 15:10:24 -0300

    "arrumando a rota do usuario"

--- Web/InovaEdu/urls.py
+++ Web/InovaEdu/urls.py
@@ -40,10 +40,10 @@ urlpatterns = [
     # Coordenação
     
     path("coordenacao/", views.HomeCoord, name="HomeCoord"),
-    path('usuarios/', views.UsuarioCoord, name='UsuarioCoord'),
+    path('usuarios/', views.UsuarioCoord, name='UsuariosCoord'),
     path('CursoCoord/', views.CursoCoord, name='CursoCoord'),
     path('TurmaCoord/', views.TurmaCoord, name='TurmaCoord'),   
-    path("cursos/", views.CursoCoord, name="CursoCoord"),
+
 
     path('criar_curso/', views.criar_curso, name='criar_curso'),
     path('criar_turma/', views.criar_turma, name='criar_turma'),
