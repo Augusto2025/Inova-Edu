b33b43dadec727a979710355d9ea43137851ca48
augusto <augustogabrielmsousa@gmail.com>
2026-08-13 15:12:30 -0300

    "debug url"

--- Web/config/settings.py
+++ Web/config/settings.py
@@ -21,10 +21,10 @@ cloudinary.config(
 # --- Segurança ---
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-' + secrets.token_urlsafe(50))
 django_debug = os.environ.get('DJANGO_DEBUG')
-if django_debug == "True" or django_debug is not None:
-    DEBUG = True
-else:
+if django_debug == "False":
     DEBUG = False
+elif django_debug == "True" or django_debug is not None:
+    DEBUG = True
 print(f"[DJANGO DEBUG] {DEBUG}")
 ALLOWED_HOSTS = ["inova-edu.onrender.com", "127.0.0.1", "localhost"]
 
