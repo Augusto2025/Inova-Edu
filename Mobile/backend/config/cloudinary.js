5d291c8c5b997d6af55e806c90c23545a8e7346e
cidoca1217 <mneto4182@hotmail.com>
2026-08-17 17:50:43 -0300

    "foto "

--- Mobile/backend/config/cloudinary.js
+++ Mobile/backend/config/cloudinary.js
@@ -1,5 +1,10 @@
 const { v2: cloudinary } = require('cloudinary');
 
+console.log("☁️ CLOUDINARY:");
+console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY:", process.env.CLOUDINARY_API_KEY ? "OK" : "NÃO ENCONTRADA");
+console.log("API SECRET:", process.env.CLOUDINARY_API_SECRET ? "OK" : "NÃO ENCONTRADA");
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
